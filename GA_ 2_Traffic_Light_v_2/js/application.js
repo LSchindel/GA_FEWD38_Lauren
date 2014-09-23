06da103f11440a331625206d6120a4a9bc6aa7f9
Saimon Sharif <sharif.saimon@gmail.com>
2014-09-23 18:12:01 -0400

    Help with traffic light JS

--- GA_ 2_Traffic_Light_v_2/js/application.js
+++ GA_ 2_Traffic_Light_v_2/js/application.js
@@ -1,6 +1,11 @@
 function click(turnOn){
 var go = document.getElementByClass("green");
 
-var lightOn = document.getElementByclass(turnOn);
+// Watch out! You should doublecheck, but getElementByClass returns an array
+var lightOn = document.getElementByClass(turnOn);
+
+// We don't actually have a variable for light yet
 light.style.backgroundColor = turnOn;
+
+
 }
