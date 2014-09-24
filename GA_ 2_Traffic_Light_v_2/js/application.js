5ee89f6e42be3d9d2cb9cd26c0bbc54ceb48a960
LSchindel <laurenschindel@laurensndelsair.home>
2014-09-23 21:45:12 -0400

    created array

--- GA_ 2_Traffic_Light_v_2/js/application.js
+++ GA_ 2_Traffic_Light_v_2/js/application.js
@@ -1,11 +1,10 @@
 function click(turnOn){
-var go = document.getElementByClass("green");
+var go = document.getElementByClass('green');
 
 // Watch out! You should doublecheck, but getElementByClass returns an array
-var lightOn = document.getElementByClass(turnOn);
-
+var light = [ 'green', 'yellow', 'red'];
+document.getElementByClass('light') = light;
+ 
 // We don't actually have a variable for light yet
 light.style.backgroundColor = turnOn;
-
-
 }
