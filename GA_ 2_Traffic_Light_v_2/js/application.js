b50850db0a2b948194bd647bcf63f9bbf6189ba8
LSchindel <laurenschindel@laurensndelsair.home>
2014-09-24 21:40:14 -0400

    pseudo code

--- GA_ 2_Traffic_Light_v_2/js/application.js
+++ GA_ 2_Traffic_Light_v_2/js/application.js
@@ -8,3 +8,18 @@ document.getElementByClass('light') = light;
 // We don't actually have a variable for light yet
 light.style.backgroundColor = turnOn;
 }
+
+
+// On open all lights are off
+
+// Click button and 1 light turns on
+
+// if 1 light is on, turn off all other lights
+
+// Make all buttons are active
+
+// Click another button and light turns on
+
+// if 1 light is on, turn off all other lights
+
+
