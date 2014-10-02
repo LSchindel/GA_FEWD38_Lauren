5c82c5b416d604831b118fb232e44075e347159c
LSchindel <laurenschindel@laurensndelsair.home>
2014-10-01 22:10:05 -0400

    background color change

--- GA_3_photo_library/js/application.js
+++ GA_3_photo_library/js/application.js
@@ -1,1 +1,23 @@
-application.js
+var images = ["img/camping.jpg", "img/flower.jpg", "img/flower2.jpg", "img/cobb.jpg" ]
+var currentImage = 0;
+
+function changePicture(whichNum) {
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', images[whichNum]);
+	currentImage = whichNum;
+}
+
+function nextImage() {
+	currentImage++;
+	if(currentImage==images.length){
+		currentImage = 0;
+	}
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', images[currentImage]);
+}
+
+function prevImage() {
+	currentImage--;
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', images[currentImage]);
+}
