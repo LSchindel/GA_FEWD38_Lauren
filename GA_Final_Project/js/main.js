52986e34edc59890c19f45d68a8ef4d0abfa2cfc
LSchindel <laurenschindel@laurensndelsair.home>
2014-10-08 19:38:46 -0400

    game code along

---
+++ GA_Final_Project/js/main.js
@@ -0,0 +1,7 @@
+// user enters restaurant name and zip
+
+// user selects restaurant from the list
+
+// last inspection date, violations show
+
+// graphic recomends eating at or avoiding restuarant
