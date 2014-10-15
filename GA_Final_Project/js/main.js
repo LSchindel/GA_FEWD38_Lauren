4d01d0369ccd4f9167d203d00d0308bd8f25ebbc
LSchindel <laurenschindel@LaurenSchindels-MacBook-Air.local>
2014-10-14 20:45:47 -0400

    js set up

--- GA_Final_Project/js/main.js
+++ GA_Final_Project/js/main.js
@@ -1,7 +1,43 @@
 // user enters restaurant name and zip
 
+//user clicks submit
+function handleSubmit(){
+	//get the zipcode from the zipcode input
+
+	//get the name of the restaraunt
+
+	//call the health department!
+
+	var data; 
+	displayRestaurants(data); 
+
+}
+
+function displayRestaurants(restaurants){
+	// format data
+	// get jquery reference to the div where to display restaurant
+	// inject restaurants into the div
+
+}
 // user selects restaurant from the list
+
+function selectRestaurant() {
+	// choose restaruant
+	// get json to retrive information
+
+}
 
 // last inspection date, violations show
 
+function showViolations() {
+	// use get json to select violations
+	// format information
+	// show violations
+	// show last inspection date
+}
+
 // graphic recomends eating at or avoiding restuarant
+
+function showGraphic(){
+
+}
